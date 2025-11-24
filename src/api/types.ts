6b4bf9541a34f1ac3722ceec14684d17e5a7299c
wtcpython <1762993226@qq.com>
2025-11-24 13:18:52 +0800

    Init

---
+++ src/api/types.ts
@@ -0,0 +1,17 @@
+export interface ApiResponse<T = any> {
+  code: number
+  msg: string
+  data: T
+}
+
+export interface LoginForm {
+  username: string
+  password: string
+}
+
+export interface LoginToken {
+  id: number
+  username: string
+  name: string
+  token: string
+}
